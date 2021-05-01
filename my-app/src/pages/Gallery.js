4689fe1d39981bcd1c3cad80b9ff37f124fe991b
CF Yang <cyang22@bloomberg.net>
2021-05-01 12:05:04 -0500

    Start basic flex and mark TODO.

--- my-app/src/pages/Gallery.js
+++ my-app/src/pages/Gallery.js
@@ -6,25 +6,38 @@ import "../styles/Gallery.css";
 class Gallery extends Component {
     constructor(props) {
         super(props)
+        /* TODO: See "Rendering Dynamic Data" at https://www.pluralsight.com/guides/how-to-implement-a-component-%22loop%22-with-react
 
+        Fetch the list from the backend and set the projects list.
+        */
         this.state = {
-            
+            projects: [
+                { title: 'The Gallery', author: 'The Curators' },
+                { title: "Weather App", author: "Zeus" }
+            ]
         }
     }
 
     render() {
+        let projectList = []
+        this.state.projects.forEach(project => {
+            projectList.push(<li class="flex-item">
+                <Project
+                    title={project.title}
+                    author={project.author}
+                >
+                </Project></li>)
+        })
+
         return (
             <div className="root">
-                <Banner 
+                <Banner
                     text="The Knowledge House"
                 />
                 <h1> Gallery </h1>
-                <Project 
-                    title="new Project"
-                    name="Sandeep"
-                    link="google.com"
-                >
-                </Project>
+                <ul class="flex-container">
+                    {projectList}
+                </ul>
             </div>
         )
     }
